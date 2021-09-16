1dc737a68d5c5496dbc890870bd99e45e709d31d
Muhammad Arif <45303232+ArifSetiyawan@users.noreply.github.com>
2021-09-17 01:57:38 +0700

    first-templating

---
+++ application/views/home.php
@@ -0,0 +1,185 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+	<!-- Content Header (Page header) -->
+	<div class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1 class="m-0 text-dark">Dashboard</h1>
+				</div><!-- /.col -->
+				<div class="col-sm-6">
+					<ol class="breadcrumb float-sm-right">
+						<li class="breadcrumb-item"><a href="#">Home</a></li>
+						<li class="breadcrumb-item active">Dashboard v1</li>
+					</ol>
+				</div><!-- /.col -->
+			</div><!-- /.row -->
+		</div><!-- /.container-fluid -->
+	</div>
+	<!-- /.content-header -->
+
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<marquee behavior="alternate">
+				<h2 style="color: black;"><b>Selamat datang di Rental Mobil UBSI MARGONDA</b></h2>
+			</marquee>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="card card-danger">
+						<div class="card-header">
+							<div class="card-title">
+								My Teams
+							</div>
+						</div>
+						<div class="card-body pb-0">
+							<div class="row">
+								<div class="col-sm-4">
+									<div class="card bg-light">
+										<div class="card-header text-muted border-bottom-0">
+											12.2D.01 / 12200456
+										</div>
+										<div class="card-body pt-0">
+											<div class="row">
+												<div class="col-7">
+													<h2 class="lead"><b>Farhana</b></h2>
+													<p class="text-muted text-sm"></p>
+													<ul class="ml-4 mb-0 fa-ul text-muted">
+														<li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
+														<li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
+													</ul>
+												</div>
+												<div class="col-5 text-center">
+													<img src="../public/asset/images/user-1.jpeg" alt="" class="img-circle img-fluid">
+												</div>
+											</div>
+										</div>
+										<div class="card-footer">
+											<div class="text-right">
+												<a href="#" class="btn btn-sm bg-teal">
+													<i class="fas fa-comments"></i>
+												</a>
+												<a href="#" class="btn btn-sm btn-primary">
+													<i class="fas fa-user"></i> View Profile
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="card bg-light">
+										<div class="card-header text-muted border-bottom-0">
+											12.2D.01 / 12200943
+										</div>
+										<div class="card-body pt-0">
+											<div class="row">
+												<div class="col-7">
+													<h2 class="lead"><b>Muhammad Arif Setiyawan</b></h2>
+													<p class="text-muted text-sm"></p>
+													<ul class="ml-4 mb-0 fa-ul text-muted">
+														<li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
+														<li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
+													</ul>
+												</div>
+												<div class="col-5 text-center">
+													<img src="../public/asset/images/user-2.jpeg" alt="" class="img-circle img-fluid">
+												</div>
+											</div>
+										</div>
+										<div class="card-footer">
+											<div class="text-right">
+												<a href="#" class="btn btn-sm bg-teal">
+													<i class="fas fa-comments"></i>
+												</a>
+												<a href="#" class="btn btn-sm btn-primary">
+													<i class="fas fa-user"></i> View Profile
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="card bg-light">
+										<div class="card-header text-muted border-bottom-0">
+											12.2D.01 / 12200064
+										</div>
+										<div class="card-body pt-0">
+											<div class="row">
+												<div class="col-7">
+													<h2 class="lead"><b>Muhammad Daniel Nurindra</b></h2>
+													<p class="text-muted text-sm"></p>
+													<ul class="ml-4 mb-0 fa-ul text-muted">
+														<li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
+														<li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
+													</ul>
+												</div>
+												<div class="col-5 text-center">
+													<img src="../public/asset/images/user-2.jpeg" alt="" class="img-circle img-fluid">
+												</div>
+											</div>
+										</div>
+										<div class="card-footer">
+											<div class="text-right">
+												<a href="#" class="btn btn-sm bg-teal">
+													<i class="fas fa-comments"></i>
+												</a>
+												<a href="#" class="btn btn-sm btn-primary">
+													<i class="fas fa-user"></i> View Profile
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="card card-primary">
+						<div class="card-header">
+							<div class="card-title">
+								Gallery
+							</div>
+						</div>
+						<div class="card-body">
+							<div class="row">
+								<div class="col-sm-4">
+									<a href="#" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+										<img src="<?php echo base_url(); ?>assets/images/mobil-1.jpg" class="img-fluid mb-2" alt="white sample" />
+									</a>
+								</div>
+								<div class="col-sm-4">
+									<a href="#" data-toggle="lightbox" data-title="sample 2 - black" data-gallery="gallery">
+										<img src="<?php echo base_url(); ?>assets/images/mobil-8.jpeg" class="img-fluid mb-2" alt="black sample" />
+									</a>
+								</div>
+								<div class="col-sm-4">
+									<a href="#" data-toggle="lightbox" data-title="sample 3 - red" data-gallery="gallery">
+										<img src="<?php echo base_url(); ?>assets/images/mobil-2.jpeg" class="img-fluid mb-2" alt="red sample" />
+									</a>
+								</div>
+								<div class="col-sm-4">
+									<a href="#" data-toggle="lightbox" data-title="sample 4 - red" data-gallery="gallery">
+										<img src="<?php echo base_url(); ?>assets/images/mobil-3.jpeg" class="img-fluid mb-2" alt="red sample" />
+									</a>
+								</div>
+								<div class="col-sm-4">
+									<a href="#" data-toggle="lightbox" data-title="sample 5 - black" data-gallery="gallery">
+										<img src="<?php echo base_url(); ?>assets/images/mobil-6.jpeg" class="img-fluid mb-2" alt="black sample" />
+									</a>
+								</div>
+								<div class="col-sm-4">
+									<a href="#" data-toggle="lightbox" data-title="sample 5 - black" data-gallery="gallery">
+										<img src="<?php echo base_url(); ?>assets/images/mobil-7.jpeg" class="img-fluid mb-2" alt="black sample" />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
