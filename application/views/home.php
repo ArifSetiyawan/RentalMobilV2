15c6ef5ebdb07d353e9af8fe0659db7595ba4337
BarrenLand <danielkevinlangley@live.com>
2021-11-28 13:51:46 +0900

    push

--- application/views/home.php
+++ application/views/home.php
@@ -95,27 +95,19 @@
 									<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 									<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
 								</ol>
 								<div class="carousel-inner">
 									<div class="carousel-item active">
 										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-1.jpg">
 									</div>
 									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-8.jpeg">
+										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-2.jpg">
 									</div>
 									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-2.jpeg">
+										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-3.jpg">
 									</div>
 									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-3.jpeg">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-6.jpeg">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-7.jpeg">
+										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-4.jpg">
 									</div>
 								</div>
 								<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
