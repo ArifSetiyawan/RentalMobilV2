19bb8b9e89b5f44b6d6d46806cf57b370c9a58a9
BarrenLand <danielkevinlangley@live.com>
2021-12-04 14:32:47 +1100

    test1

--- application/views/home.php
+++ application/views/home.php
@@ -4,13 +4,40 @@
 	<div class="content-header">
 		<div class="container-fluid">
 			<div class="row mb-2">
-				<div class="col-sm-6">
-					<h1 class="m-0 text-dark">Dashboard</h1>
-				</div><!-- /.col -->
-				<div class="col-sm-6">
-					<ol class="breadcrumb float-sm-right">
-						<li class="breadcrumb-item"><a href="#">Home</a></li>
-						<li class="breadcrumb-item active">Dashboard v1</li>
+				<div class="col-sm-12">
+					<div class="card-body">
+						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+							<ol class="carousel-indicators">
+								<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+							</ol>
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-1.jpg">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-2.jpg">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-3.jpg">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-4.jpg">
+								</div>
+							</div>
+							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+					</div>
+					
 					</ol>
 				</div><!-- /.col -->
 			</div><!-- /.row -->
@@ -22,143 +49,7 @@
 	<section class="content">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-12 col-sm-6 col-md-3">
-					<div class="info-box">
-						<span class="info-box-icon bg-info elevation-1"><i class="ion ion-model-s"></i></span>
-
-						<div class="info-box-content">
-							<span class="info-box-text">Data Mobil</span>
-							<span class="info-box-number">
-								10
-							</span>
-						</div>
-						<!-- /.info-box-content -->
-					</div>
-					<!-- /.info-box -->
-				</div>
-				<!-- /.col -->
-				<div class="col-12 col-sm-6 col-md-3">
-					<div class="info-box mb-3">
-						<span class="info-box-icon bg-danger elevation-1"><i class="ion ion-person"></i></span>
-
-						<div class="info-box-content">
-							<span class="info-box-text">Data Users</span>
-							<span class="info-box-number">41,410</span>
-						</div>
-						<!-- /.info-box-content -->
-					</div>
-					<!-- /.info-box -->
-				</div>
-				<!-- /.col -->
-
-				<!-- fix for small devices only -->
-				<div class="clearfix hidden-md-up"></div>
-
-				<div class="col-12 col-sm-6 col-md-3">
-					<div class="info-box mb-3">
-						<span class="info-box-icon bg-success elevation-1"><i class="ion ion-person-stalker"></i></span>
-
-						<div class="info-box-content">
-							<span class="info-box-text">Data Pelanggan</span>
-							<span class="info-box-number">760</span>
-						</div>
-						<!-- /.info-box-content -->
-					</div>
-					<!-- /.info-box -->
-				</div>
-				<!-- /.col -->
-				<div class="col-12 col-sm-6 col-md-3">
-					<div class="info-box mb-3">
-						<span class="info-box-icon bg-warning elevation-1"><i class="ion ion-clipboard"></i></span>
-
-						<div class="info-box-content">
-							<span class="info-box-text">Data Transaksi</span>
-							<span class="info-box-number">2,000</span>
-						</div>
-						<!-- /.info-box-content -->
-					</div>
-					<!-- /.info-box -->
-				</div>
-				<!-- /.col -->
-			</div>
-			<div class="row">
 				<div class="col-sm-12">
-					<div class="card card-info">
-						<div class="card-header">
-							<h3 class="card-title">The Cars</h3>
-						</div>
-						<!-- /.card-header -->
-						<div class="card-body">
-							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-								<ol class="carousel-indicators">
-									<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-								</ol>
-								<div class="carousel-inner">
-									<div class="carousel-item active">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-1.jpg">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-2.jpg">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-3.jpg">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" height="500" src="<?php echo base_url(); ?>assets/images/mobil-4.jpg">
-									</div>
-								</div>
-								<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-									<span class="sr-only">Previous</span>
-								</a>
-								<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-									<span class="carousel-control-next-icon" aria-hidden="true"></span>
-									<span class="sr-only">Next</span>
-								</a>
-							</div>
-						</div>
-						<!-- /.card-body -->
-					</div>
-
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-sm-6">
-					<div class="card card-info">
-						<div class="card-header">
-							<div class="card-title">
-								Sekilas tentang kami
-							</div>
-						</div>
-						<div class="card-body">
-							<ul class="list-unstyled">
-								<li>Web dibuat untuk menyelesaikan tugas web programming 2 
-									untuk memenuhi tugas akhir semester 3 dan sebagai pengganti uas.
-									Web ini juga berfungsi sebagai portofolio bagi kami untuk 
-									keperluan pribadi di masa depan.</li>
-								<p></p>
-								<li>Web ini dibuat menggunakan Codeigniter 3 sebagai basis
-									dan menggunakan basis yang hampir sama dengan tugas harian
-									kami.</li>
-								<p></p>
-								<li>Asset yang kami gunakan kebanyakan kami ambil dari Drivetribe, StanceNation, Shutterstock, dan Autobild</li>
-								<p></p>
-								<br></br>
-								<p></p>
-								<br></br>
-								<p></p>
-								<br></br>
-								<br></br>
-								<br></br>
-								<br></br>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-6">
 					<div class="card card-primary">
 						<div class="card-header">
 							<div class="card-title">
