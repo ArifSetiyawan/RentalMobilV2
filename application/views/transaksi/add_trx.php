75e2a9d7c4ac1a6f6180e5a1214c98ca77a568cd
BarrenLand <danielkevinlangley@live.com>
2022-04-04 12:14:18 +0700

    thumb_update

--- application/views/transaksi/add_trx.php
+++ application/views/transaksi/add_trx.php
@@ -46,8 +46,6 @@
                                         <span class="username">
                                             <?php echo $mobil['merek'] ?> <?php echo $mobil['nama_mobil'] ?>
                                         </span>
-                                        It is a long established fact that a reader will be distracted
-                                        by the readable content of a page when looking at its layout.
                                     </div>
 
                                 </div>
